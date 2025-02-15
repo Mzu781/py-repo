0eb18dc94c0ef49c227a702eb147d05d7d122ed7
Mzu781 <muzuvukile.nqwiliso@sambeconsulting.com>
2025-02-15 19:14:24 +0200

    modified py file

--- import.py
+++ import.py
@@ -1,3 +1,11 @@
 print('hello editor')
 list_number = [3,4,5]
-print(dir(list_number))
+# print(dir(list_number))
+list_number.append(20)
+list_number.sort(reverse=True)
+print(list_number)
+sum = 0
+for num in list_number:
+    sum+=num
+
+print(f"sum = {sum}")
